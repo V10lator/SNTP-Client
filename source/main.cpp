57df6bf3babf07974c4f41d1fffc05514febd7e1
Thomas Rohloff <v10lator@myway.de>
2023-10-03 12:06:02 +0200

    v1.12
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- source/main.cpp
+++ source/main.cpp
@@ -43,7 +43,7 @@
 // Important plugin information.
 WUPS_PLUGIN_NAME("SNTP Client");
 WUPS_PLUGIN_DESCRIPTION("A plugin that synchronizes a Wii U's clock with SNTP.");
-WUPS_PLUGIN_VERSION("v1.11");
+WUPS_PLUGIN_VERSION("v1.12");
 WUPS_PLUGIN_AUTHOR("Nightkingale & V10lator");
 WUPS_PLUGIN_LICENSE("MIT");
 
